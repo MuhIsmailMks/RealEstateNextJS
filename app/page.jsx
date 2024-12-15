9ee060b1811afbcea9cafea24c6dd46d57b70cf6
muhammad ismail <muhismailmks@gmail.com>
2024-12-15 18:47:32 +0800

    delete unused code

--- app/page.jsx
+++ app/page.jsx
@@ -6,17 +6,7 @@ import React, {useEffect, useState} from "react";
 import Image from "next/image"; 
 import Navbar from "./components/Navbar";   
 import Hero from "./components/Hero";
-import {CategoryCard, DealsContainer, AboutCities, FaqContainer, SocialLink, FooterLink} from "./components/ComponentsContainer";  
- 
-// swiper
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import {Swiper, SwiperSlide} from 'swiper/react';  
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
+import {CategoryCard, AboutCities, FaqContainer, SocialLink, FooterLink} from "./components/ComponentsContainer";  
 
 import dynamic from "next/dynamic"; 
 
@@ -262,82 +252,8 @@ export default function Home() {
         
         {/* deals container */}
         <div className="flex_center relative w-full h-auto">
-
-        {/* <Swiper 
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        className="max-w-[1900px]" 
-        slidesPerView={3}
-        breakpoints={{
-        0: {
-          slidesPerView: 1,
-        },
-        400: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
-      }} 
-      pagination={{ clickable: true }} 
-      onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={(i) => console.log('slide change')}
-      >
-          <SwiperSlide>
-
-            <DealsContainer
-            imageSrc={'/images/deals1.webp'}
-            title='Skyper Pool Apartment'
-            location='1020 Bloomingdale Ave'
-            bed='4'
-            bathroom='2'
-            wide='450'
-            price='$280,000'
-            aboutDeals='For Sale'
-            />
-
-          </SwiperSlide>
-          <SwiperSlide>
-            
-            
-          <DealsContainer
-            imageSrc={'/images/deals2.webp'}
-            title='North Dillard Street'
-            location='4330 Bell Shoals Rd'
-            bed='4'
-            bathroom='2'
-            wide='400'
-            price='$250'
-            isRent={true}
-            aboutDeals='For Rent'
-            />
-
-          </SwiperSlide>
-
-          <SwiperSlide>
-
-              <DealsContainer
-                imageSrc={'/images/deals3.webp'}
-                title='Eaton Garth Penthouse'
-                location='7722 18th Ave, Brooklyn'
-                bed='4'
-                bathroom='2'
-                wide='450'
-                price='$180,000'
-                aboutDeals='For Sale'
-                isFeatured={true}
-                />
-
-          </SwiperSlide>
-
-          <SwiperSlide>Slide 4</SwiperSlide>
-
-          <SwiperSlide>Slide 5</SwiperSlide>
-
-          <SwiperSlide>Slide 6</SwiperSlide>
-     
-       </Swiper> */}
-       
-       <SwiperDeals1/>
+ 
+            <SwiperDeals1/>
           
         </div>
 
